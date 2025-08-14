f4ca364f57941c8977e22bef7b302d95f59f2bda
Ibrohiym <ibrohiym060@gmail.com>
2025-08-14 20:26:49 +0500

    banner,delivery

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,13 @@ function Banner() {
   return (
     <div className='banner'>
 
-      banner
+      <div className="banner-left">
+        <h1> Купите автомобиль <br /> дешевле на 60%* </h1>
+        <p> Привезем автомобиль под ключ со всеми документами. <br /> Подбор автомобиля под любой запрос  БЕСПЛАТНО   </p>
+        <button> Оставить заявку </button>
+      </div>
+
+      <img src={e34} className="e34" alt="" />
 
     </div>
   )
