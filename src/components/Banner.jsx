2e5f76a41fbe0c54e469ab6bd42a2d6ce736bdb2
Omadbek080804 <odilovo575@gmail.com>
2025-08-14 19:26:40 +0500

    nav

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div>
+    <div className='banner'>
       banner
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
+import bmw from '../assets/images/BMW.png'
+import ww from '../assets/images/ww.svg'
+import mers from '../assets/images/Merc.png'
+import { FaTelegram } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
 function Navbar() {
-  return (
-    <div>
-      nav
-    </div>
-  )
+    return (
+        <nav >
+
+
+            <div className="left">
+                <img src={bmw} className='ww' alt="w" />
+                <img src={ww} className='ww2' alt="s" />
+                <img src={mers} className='ww' alt="e" />
+            </div>
+            <ul>
+                <li>
+                    <a href="#"> О нас</a>
+                </li>
+                <li>
+                    <a href="#">Услуги</a>
+                </li>
+                <li>
+                    <a href="#"> Как купить</a>
+                </li>
+                <li>
+                    <a href="#"> Преимущества</a>
+                </li>
+                <li>
+                    <a href="#"> Отзывы</a>
+                </li>
+                <li>
+                    <a href="#">Контакты</a>
+                </li>
+            </ul>
+            <div className="right">
+                <FaTelegram className='icon' />
+                <FaInstagram  className='icon'/>
+                <FaYoutube className='icon' />
+                <div className="tel">
+                    <h1>+ 998 99 859 00 37</h1>
+                    <h1>+ 998 99 859 00 38</h1>
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
